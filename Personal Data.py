ceba44a44bf7d8e62492667ea4cc7e2cc9de3768
romey2991-cyber <romey2991@gmail.com>
2025-10-30 15:09:28 +0700

    Update Personal Data.py

--- Personal Data.py
+++ Personal Data.py
@@ -1,9 +1,1 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        result = []
-        for a, b in zip(word1, word2):
-            result.append(a)
-            result.append(b)
-        result.append(word1[len(word2):])
-        result.append(word2[len(word1):])
-        return ''.join(result)
+
